439b23c9ec76a432d7009bd9f5cc04134ac703e3
Zachary Williams <kcaz311@gmail.com>
2021-04-05 18:59:33 -0400

    index and show

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,20 @@
+class CouponsController < ApplicationController
+
+def index
+    @coupons = Coupons.all
+end
+
+def show
+    @coupon = Coupon.find(params[:id])
+end
+
+def new
+
+end
+
+def create
+
+end
+
+
+end
